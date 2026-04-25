b8b3621aa8778935e01eea6ed5c90e78e44dcc57
Orhan Gök <orhangok71@gmail.com>
2026-04-25 20:53:22 +0300

    güncelleme

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-document.querySelectorAll(".gallery img").forEach(img => {
-  img.addEventListener("click", function () {
-    let mainImage = this.closest(".card").querySelector(".main-img");
-    mainImage.src = this.src;
+document.querySelectorAll(".card").forEach(function(card) {
+  const mainImg = card.querySelector(".main-img");
+  const thumbnails = card.querySelectorAll(".gallery img");
+  const prevBtn = card.querySelector(".prev");
+  const nextBtn = card.querySelector(".next");
+
+  let currentIndex = 0;
+
+  function showImage(index) {
+    if (!mainImg || thumbnails.length === 0) return;
+
+    if (index < 0) {
+      currentIndex = thumbnails.length - 1;
+    } else if (index >= thumbnails.length) {
+      currentIndex = 0;
+    } else {
+      currentIndex = index;
+    }
+
+    mainImg.src = thumbnails[currentIndex].src;
+  }
+
+  thumbnails.forEach(function(img, index) {
+    img.addEventListener("click", function() {
+      showImage(index);
+    });
   });
+
+  if (prevBtn) {
+    prevBtn.addEventListener("click", function() {
+      showImage(currentIndex - 1);
+    });
+  }
+
+  if (nextBtn) {
+    nextBtn.addEventListener("click", function() {
+      showImage(currentIndex + 1);
+    });
+  }
 });
