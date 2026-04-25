c443d593c12009f987e70ce7125f700573124a14
Orhan Gök <orhangok71@gmail.com>
2026-04-25 17:59:16 +0300

    fotoğraf slider

--- script.js
+++ script.js
@@ -1,27 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll(".btn");
-
-  buttons.forEach(function (btn) {
-    btn.addEventListener("click", function () {
-      console.log("WhatsApp butonuna tıklandı");
-    });
-  });
-
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach(function (card) {
-    card.style.opacity = "1";
-    card.style.transform = "translateY(0)";
-  });
-
-  const galleryImages = document.querySelectorAll(".gallery img");
-
-  galleryImages.forEach(function (img) {
-    img.addEventListener("click", function () {
-      const card = this.closest(".card");
-      const mainImage = card.querySelector(".main-img");
-
-      mainImage.src = this.src;
-    });
+document.querySelectorAll(".gallery img").forEach(img => {
+  img.addEventListener("click", function () {
+    let mainImage = this.closest(".card").querySelector(".main-img");
+    mainImage.src = this.src;
   });
 });
